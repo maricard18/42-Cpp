2b7af0dc98c39411d9368051fcc1d86a92a1e370
Mario <mariojunior200218@gmail.com>
2023-07-05 10:38:35 +0100

    ex02

--- Cpp00/ex02/Account.cpp
+++ Cpp00/ex02/Account.cpp
@@ -6,17 +6,19 @@
 /*   By: maricard <maricard@student.porto.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 16:38:58 by maricard          #+#    #+#             */
-/*   Updated: 2023/07/03 19:10:02 by maricard         ###   ########.fr       */
+/*   Updated: 2023/07/04 11:22:43 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Account.hpp"
 #include <iostream>
+#include <iomanip>
+#include <ctime>
+#include "Account.hpp"
 
-int	_accountIndex = 0;
-int	_amount = 0;	
-int	_nbDeposits = 0;
-int	_nbWithdrawals = 0;
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
 int Account::getNbAccounts( void )
 {
@@ -55,16 +57,31 @@ void Account::makeDeposit( int deposit )
 bool	Account::makeWithdrawal( int withdrawal )
 {	
 	(void)withdrawal;
+	return (true);
 }
 
 int		Account::checkAmount( void ) const
 {
+	return 1;
 }
 
 void	Account::displayStatus( void ) const
 {
 }
 
-void Account::_displayTimestamp( void )
+void Account :: _displayTimestamp( void )
 {
+	std::time_t	time_in_sec;
+	std::tm 	*time;
+
+	time_in_sec = std::time(NULL);
+	time = std::localtime(&time_in_sec);
+	int year = time->tm_year + 1900;
+	int month = time->tm_mon;
+	int day = time->tm_mday;
+	int hour = time->tm_hour;
+	int min = time->tm_min;
+	int sec = time->tm_sec;
+	
+	std::cout << "[" << year << month << day << "_" << hour << min << sec << "] ";
 }
