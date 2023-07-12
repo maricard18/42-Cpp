86389cedc2fc32784287e79fa406cdf66c3c1657
maricard <maricard@student.porto.com>
2023-07-12 20:23:50 +0100

    starting cpp02

---
+++ Cpp02/ex00/Fixed.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maricard <maricard@student.porto.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 18:47:52 by maricard          #+#    #+#             */
+/*   Updated: 2023/07/12 19:01:14 by maricard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <iomanip>
+
+class Fixed
+{
+	private:
+		int value;
+		static int bits;
+
+	public:
+
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+	
+		//! Constructors and destructor
+		Fixed();
+		Fixed(const Fixed& copy);
+		~Fixed();
+
+		//! Operator overloading
+		Fixed& operator=(const Fixed& other);
+};
+
+#endif
---
+++ Cpp02/ex00/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maricard <maricard@student.porto.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 18:47:11 by maricard          #+#    #+#             */
+/*   Updated: 2023/07/12 18:59:19 by maricard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "File.hpp"
+#include <iostream>
+
+int main( void )
+{
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	
+	c = b;
+	
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return 0;
+}
