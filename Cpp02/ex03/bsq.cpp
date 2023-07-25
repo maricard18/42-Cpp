d3bef1d4458532e92f74ba162a8145e038111fcf
maricard <maricard@student.porto.com>
2023-07-25 13:49:09 +0100

    ex03: ongoing

--- Cpp02/ex03/bsq.cpp
+++ Cpp02/ex03/bsq.cpp
@@ -6,7 +6,7 @@
 /*   By: maricard <maricard@student.porto.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 18:04:20 by maricard          #+#    #+#             */
-/*   Updated: 2023/07/25 13:05:01 by maricard         ###   ########.fr       */
+/*   Updated: 2023/07/25 13:48:50 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ Fixed setArea(Point const a, Point const b, Point const c)
 	Fixed w;
 
 	w = (a.getX() * (b.getY() - c.getY()) + b.getX() * (c.getY() - a.getY()) + c.getX() * (a.getY() - b.getY()));
+	std::cout << "w = " << w << std::endl;
 	if (w < 0)
 		w = w * -1;
 	w = w * 0.5f;
-	std::cout << "w = " << w << std::endl;
 	return (w);
 }
 
