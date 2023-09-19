a54d45d5bad1df400c99807ce454881c3b779cac
maricard <maricard@student.porto.com>
2023-09-19 20:23:47 +0100

    Cpp07 ex01: exercise started

--- Cpp07/ex01/iter.hpp
+++ Cpp07/ex01/iter.hpp
@@ -6,7 +6,33 @@
 /*   By: maricard <maricard@student.porto.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 20:06:12 by maricard          #+#    #+#             */
-/*   Updated: 2023/09/19 20:06:13 by maricard         ###   ########.fr       */
+/*   Updated: 2023/09/19 20:23:01 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <iostream>
+
+# define RED "\033[1;31m"
+# define GREEN "\033[1;32m"
+# define RESET "\033[0m"
+
+template <typename T>
+void 	swap(T &x, T &y)
+{
+    T temp;
+
+	temp = x;
+	x = y;
+	y = temp;
+}
+
+template <typename T, typename F>
+void	iter(T& array, size_t len, U function)
+{
+	
+}
+
+#endif
